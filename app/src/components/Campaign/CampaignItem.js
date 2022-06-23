ee540b8521f14532bd508137dc2214fcff99f046
Siuhin <hin07012@gmail.com>
2022-06-23 21:08:28 +0800

    Style: Render basic style of campaign item

--- app/src/components/Campaign/CampaignItem.js
+++ app/src/components/Campaign/CampaignItem.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
 
-export default function CampaignItem({item}) {
+import linkImg from "static/link.svg";
+import downloadImg from "static/download.svg";
+
+import "./Campaign.css";
+
+export default function CampaignItem({ item }) {
   return (
-    <div>CampaignItem</div>
-  )
+    <>
+      <div className="descriptionContainer">
+        <img
+          src={item.campaign_icon_url}
+          alt={`${item.campaign_name}`}
+          className="icon"
+        />
+        <div className="description">
+          <span className="name">{item.campaign_name}</span>
+          <span className="preInstall">
+            <b>{item.pay_per_install}</b> pre install
+          </span>
+        </div>
+      </div>
+      <div className="mediaContainer">
+        {item.medias.map((media, idx) => (
+          <div key={idx} className="mediaItem">
+            <img src={media.cover_photo_url} alt="media" className="coverPic" />
+            <div className="btnGroup">
+              <div className="btn">
+                <img src={linkImg} alt="Link button" />
+              </div>
+              <div className="btn">
+                <img src={downloadImg} alt="Download button" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
