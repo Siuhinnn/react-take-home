ed150a9a6d19c822f9fbe2da426d246746751bcc
Siuhin <hin07012@gmail.com>
2022-06-23 16:21:34 +0800

    Feat: new fetch function
    Fetch data from endpoint using fetch() method and store in state in App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [dataArray, setDataArray] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://www.plugco.in/public/take_home_sample_feed"
+        );
+        const result = await response.json();
+        console.log(result);
+        setDataArray(result.campaigns);
+        setErrorMsg(null);
+      } catch (err) {
+        setErrorMsg("Failed to fetch");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <></>;
 }
 
 export default App;
