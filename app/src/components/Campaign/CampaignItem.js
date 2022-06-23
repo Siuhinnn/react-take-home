a239f1dfb2b867a481f714df0472fa9b0f2c4bc1
Siuhin <hin07012@gmail.com>
2022-06-23 21:10:30 +0800

    Style: Add play button
    Add the play button and backdrop when the media is a video

--- app/src/components/Campaign/CampaignItem.js
+++ app/src/components/Campaign/CampaignItem.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import linkImg from "static/link.svg";
 import downloadImg from "static/download.svg";
+import playIcon from "static/play.svg";
 
 import "./Campaign.css";
 
@@ -25,6 +26,11 @@ export default function CampaignItem({ item }) {
         {item.medias.map((media, idx) => (
           <div key={idx} className="mediaItem">
             <img src={media.cover_photo_url} alt="media" className="coverPic" />
+            {media.media_type === "video" && (
+              <div className="mediaBackdrop">
+                <img src={playIcon} alt="Play button" className="playIcon" />
+              </div>
+            )}
             <div className="btnGroup">
               <div className="btn">
                 <img src={linkImg} alt="Link button" />
